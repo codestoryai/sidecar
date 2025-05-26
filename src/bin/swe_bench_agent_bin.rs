e55a69088be268f3c78d424fc52382735a536abe
chojuninengu <chojuninengu@gmail.com>
2025-05-26 12:48:35 +0100

    feat: add ToolType variants for file operations in swe_bench_agent_bin.rs

---
+++ src/bin/swe_bench_agent_bin.rs
@@ -0,0 +1,13 @@
+let tools = vec![
+    ToolType::ListFiles,
+    ToolType::SearchFileContentWithRegex,
+    ToolType::OpenFile,
+    ToolType::CodeEditing,
+    ToolType::AttemptCompletion,
+    ToolType::TerminalCommand,
+    ToolType::FindFiles,
+    ToolType::GitCommit,
+    ToolType::GitCreateBranch,
+    ToolType::GitPush,
+    ToolType::GitCreatePullRequest,
+]; 
